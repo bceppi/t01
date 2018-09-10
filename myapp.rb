973d7e1c6ba99fbf848e0e48c3ec75a7258df0ce
Bastian Ceppi <beceppi@uc.cl>
2018-09-09 22:26:51 -0300

    Added config and landing resources

--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,21 @@
 require "rubygems"
 require "sinatra/base"
+require 'sinatra/activerecord'
+# require 'sinatra/activerecord/rake'
+require Dir.pwd + "/models/comment.rb"
 
 class MyApp < Sinatra::Base
 
   get '/' do
-    'Hello, nginx and unicorn!'
+    @comment = Comment.all
+    erb :index
+  end
+
+  post '/comment' do
+    p "aqui van los params"
+    p params
+    Comment.create(content: params["content"], client_ip: request.ip)
+    redirect '/'
   end
 
 end
